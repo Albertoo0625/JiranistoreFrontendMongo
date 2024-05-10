a6f69414ec591f44982b2aaac23e4156bcfd3880
albertoo0625 <tooalbert79@gmail.com>
2024-05-10 10:46:05 +0300

    deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-export const BASE_URL = 'http://localhost:3500';
+// export const BASE_URL = 'http://localhost:3500';
+
+export const BASE_URL = 'https://jiranistorebackendmongo.onrender.com';
 
 export default axios.create({
     baseURL: BASE_URL
